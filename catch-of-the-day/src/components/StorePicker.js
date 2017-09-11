bdc4a7fe1b18c478b48368141b49b2b858bebadf
supermay <wu_jiamei@outlook.com>
2017-09-11 12:04:28 +0200

    React Event

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  goToStore(event){
+    event.preventDefault();
+    // prevent the auto refresh
+    console.log(this.storeInput.value);
+    // first grab the test from the box
+    // second change the url from / to /store/:storeId
+  }
   render(){
     return (
-    <form className="store-selector">
+    <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
       <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+      <input type="text" required placeholder="Store Name" defaultValue={getFunName()}
+      ref={(input) => { this.storeInput = input }}/>
       <button type="submit">Visit Store ⮕</button>
     </form>
     )
