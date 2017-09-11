72d48dc7b0432b8dc373367b515fc746d58202ef
supermay <wu_jiamei@outlook.com>
2017-09-11 13:19:31 +0200

    Router fixed for form submit botton

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,20 +5,28 @@ class StorePicker extends React.Component {
   goToStore(event){
     event.preventDefault();
     // prevent the auto refresh
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value
+    console.log(`Going to ${storeId}`);
     // first grab the test from the box
     // second change the url from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
   render(){
     return (
     <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      {/* pay attention to why we need to bind(this)*/}
       <h2>Please Enter A Store</h2>
       <input type="text" required placeholder="Store Name" defaultValue={getFunName()}
       ref={(input) => { this.storeInput = input }}/>
+      {/* pay attention to how to get the input from the form*/}
       <button type="submit">Visit Store ⮕</button>
     </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
