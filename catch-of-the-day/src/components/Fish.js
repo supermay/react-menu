c1e5a90f24a2f2890e188071f00b2a0965e3c347
supermay <wu_jiamei@outlook.com>
2017-09-11 15:10:43 +0200

    Display state

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+  render(){
+    const { details } = this.props;
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name}/>
+        <h3 className="fish-name">{details.name}</h3>
+        <span className="price">{formatPrice(details.price)}</span>
+        <p>{details.desc}</p>
+        <button>Add To Order</button>
+      </li>
+    )
+  }
+}
+
+export default Fish;
